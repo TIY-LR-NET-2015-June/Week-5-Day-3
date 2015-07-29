c49ef86f90b069cdd789e021d70b658a71fc8c6a
Jason W <williams.jasonmd@gmail.com>
2015-07-29 17:35:39 -0500

    Updated default route.

--- week5day3Homework/week5day3Homework/App_Start/RouteConfig.cs
+++ week5day3Homework/week5day3Homework/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace week5day3Homework
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Posts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
