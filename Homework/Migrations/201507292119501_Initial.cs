e343b9d59c4c5aa8570cadecc129e60e64bc8052
Michael Null <michael.null@bpmw.net>
2015-07-29 16:57:48 -0500

    include files

---
+++ Homework/Migrations/201507292119501_Initial.cs
@@ -0,0 +1,28 @@
+namespace Homework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 100),
+                        body = c.String(nullable: false),
+                        PubleshedOn = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
