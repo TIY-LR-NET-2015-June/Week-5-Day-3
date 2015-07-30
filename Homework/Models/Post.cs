078455cd4130881425dcef08bacdd8c0d34bfa4e
Michael Null <michael.null@bpmw.net>
2015-07-30 03:53:55 -0500

    added partial views

---
+++ Homework/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Homework.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string body { get; set; }
+
+        [Required]
+        public DateTime PubleshedOn { get; set; }
+    }
+}
